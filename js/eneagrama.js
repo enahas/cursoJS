82e1664bc7b4b132a3b449c1fcc844f16762e379
Tefi <tefinahas@gmail.com>
2021-10-07 13:56:14 -0300

    animacionesv2

--- js/eneagrama.js
+++ js/eneagrama.js
@@ -1,1 +1,3 @@
-$('#tituloForm').slideDown(3000)
+// $('#divAnimacion').hide().toggle('slow')
+$('#botonT').click(function(){
+    $('#divAnimacion').toggle('slow').css({"color":"#5E5689", "fontWeight":"bold"})})
