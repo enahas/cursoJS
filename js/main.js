bd1414f843ec7e185af9953e4dbb56c1bed040de
enahas <tefinahas@gmail.com>
2021-11-01 20:56:30 -0300

    cambios finales

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
-$(document).ready(function() {
-    $(".btn-compra").click(comprarServicio);
-});
-
-$(window).on('load',function () {
+$(window).on('load', function () {
     $("#espera").remove();
 });
 
@@ -16,28 +12,28 @@ class Servicio {
         this.descripcion = descripcion;
         this.cantidad = 1;
     }
-    subtotal(){
+    subtotal() {
         return this.cantidad * this.price;
     }
-    agregarCantidad(valor){
+    agregarCantidad(valor) {
         this.cantidad += valor;
     }
 }
 const servicios = [];
 
 $.get("../data/servicios.json", function (respuesta, estado) {
-    if(estado == "success"){
-        for (const objeto of respuesta) {            
+    if (estado == "success") {
+        for (const objeto of respuesta) {
             servicios.push(new Servicio(objeto.id, objeto.name, objeto.time, objeto.price, objeto.img, objeto.descripcion));
-        }  
-        serviciosUI(servicios, '#divDinamico');   
-    }else{
+        }
+        serviciosUI(servicios);
+    } else {
         console.log('Los datos no se cargaron correctamente');
-    }  
+    }
 });
 
-function serviciosUI(servicios, id){
-    $(id).empty();
+function serviciosUI(servicios) {
+    $('#divDinamico').empty();
     for (const servicio of servicios) {
         $('#divDinamico').append(`<div class="container d-md-block">
                                         <div class="row align-items-center m-5">
@@ -48,18 +44,16 @@ function serviciosUI(servicios, id){
                                                 <div class="row"><h5>${servicio.name}</h5></div>
                                                 <div class="row"><p>${servicio.descripcion}</p></div>
                                                 <div class="row">
-                                                    <div class="col-auto pl-0"><button class="btn btn-outline-dark btn-compra" id="${servicio.id}">Agregar</button></div>
+                                                    <div class="col-auto pl-0"><button class="btn btn-outline-dark btn-compra" id="compra${servicio.id}">Agregar</button></div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>`);
-                                    $(document).ready(function(){
-                                        $(`#${servicio.id}`).on('click',function(){
-                                            comprarServicio(`${servicio.id}`)
-                                        });
-                                    });
+        $(`#compra${servicio.id}`).on('click', function () {
+            comprarServicio(`${servicio.id}`)
+        });
     };
-  }
+}
 
 let carritoDeCompras = []
 
@@ -69,7 +63,7 @@ function comprarServicio(id) {
     if (existe == undefined) {
         carritoDeCompras.push(servicios.find(elemento => elemento.id == idServicio));
     }
-    else{
+    else {
         existe.agregarCantidad(1);
     }
     carritoUI(carritoDeCompras);
@@ -79,28 +73,28 @@ function carritoUI(servicios) {
     $("#carritoCantidad").html(servicios.length);
     $('#carrito').empty();
     for (const servicio of servicios) {
-        $('#carrito').append(`<p> ${servicio.name} <span class="badge badge-warning">$${servicio.price}</span><span class="badge badge-warning">Cantidad: ${servicio.cantidad}</span><span class="badge badge-warning">Subtotal $${servicio.subtotal()}</span></p>`);   
+        $('#carrito').append(`<p> ${servicio.name} <span class="badge badge-warning">$${servicio.price}</span><span class="badge badge-warning">Cantidad: ${servicio.cantidad}</span><span class="badge badge-warning">Subtotal $${servicio.subtotal()}</span></p>`);
     }
     $('#carrito').append(`<button class="btn btn-outline-dark btn-compra" id="btnConfirmar">Confirmar</button>`);
-    $("#btnConfirmar").on("click",enviarCompra);
+    $("#btnConfirmar").on("click", enviarCompra);
 }
 
 function enviarCompra() {
-    $.post("https://jsonplaceholder.typicode.com/posts",JSON.stringify(carritoDeCompras),function(respuesta,estado) {
-      if(estado == "success"){
-        $('#carrito').empty();
-        $('#carritoCantidad').html("0");
-        carritoDeCompras = [];
-        localStorage.clear()
-        Swal.fire({
-            icon: 'success',
-            title: 'Ok',
-            text: 'Compra Realizada!',
-            footer: '<p>N° de Orden : 5465454sd5vv54654</p>'
-          })
-      }else{
-        console.log('Los datos no se enviaron correctamente');
-      }    
-    })  
-  }
+    $.post("https://jsonplaceholder.typicode.com/posts", JSON.stringify(carritoDeCompras), function (respuesta, estado) {
+        if (estado == "success") {
+            $('#carrito').empty();
+            $('#carritoCantidad').html("0");
+            carritoDeCompras = [];
+            localStorage.clear()
+            Swal.fire({
+                icon: 'success',
+                title: 'Ok',
+                text: 'Compra Realizada!',
+                footer: '<p>N° de Orden : 5465454sd5vv54654</p>'
+            })
+        } else {
+            console.log('Los datos no se enviaron correctamente');
+        }
+    })
+}
 
